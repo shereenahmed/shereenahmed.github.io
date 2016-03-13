86a7940621308c9dc442d1efc6c1b50d24e0c00b
shereenahmed <shereen.albadry@gmail.com>
2016-03-13 12:32:36 +0100

    Fixed my star rating and some stuff in my css file

--- JS.js
+++ JS.js
@@ -16,14 +16,12 @@ $(function() {
 });
 
 
-var buttons = document.getElementsByTagName("button");
-var buttonsCount = buttons.length;
-for (var i = 0; i <= buttonsCount; i += 1) {
-    buttons[i].onclick = function(e) {
+var input = document.getElementsByTagName("input");
+var inputCount = input.length;
+for (var i = 0; i < input.length; i++) {
+    input[i].onclick = function(e) {
         alert(this.id);
     };
 }
 
 
-//var x = document.getElementById("myBtn").value;
-    //document.getElementById("demo").innerHTML = x;
